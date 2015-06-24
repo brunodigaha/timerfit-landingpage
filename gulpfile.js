6bffda5db35cd4a6c751f3389556c11f757d3394
williamalonco <william-urias@hotmail.com>
2015-06-24 02:44:05 -0300

    Configurações iniciais gulp e bower e modificações para o uso de pré-processador

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var gulp = require('gulp'),
+	sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  gulp.src('./public/css/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./public/css/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
