87526153a9ca4af592f24e4e0f15afed1b1bcd1a
williamalonco <william-urias@hotmail.com>
2015-06-27 15:43:44 -0300

    Unificação de arquivos js p/ diminuição do número de requests

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,27 @@
-'use strict';
-
 var gulp = require('gulp'),
-		sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	concat = require('gulp-concat');
 
-gulp.task('sass', function () {
-  gulp.src('./public/css/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'));
+gulp.task('sass', function() {
+	gulp.src('./public/css/sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('js', function() {
+	return gulp.src(['./public/bower_components/modernizr/modernizr.js', './public/bower_components/jquery/dist/jquery.js', './public/bower_components/materialize/dist/js/materialize.js', 'public/js/custom.js'])
+		.pipe(concat({
+			path: 'scripts.js',
+			stat: {
+				mode: 0666
+			}
+		}))
+		.pipe(gulp.dest('./public/js/'));
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./public/css/sass/**/*.scss', ['sass']);
+	gulp.watch('./public/css/sass/**/*.scss', ['sass']);
+	gulp.watch('./public/js/**/*.js', ['js']);
 });
 
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'js', 'watch']);
