320c3ff984269c9130e9230962251ec67ef3987e
williamalonco <william-urias@hotmail.com>
2015-06-28 22:54:54 -0300

    Criação da função abrir e esconder div

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,5 +3,10 @@
 		$('.button-collapse').sideNav({
 			'edge': 'left'
 		});
+		$('.outer a').on("click", function() {
+			$('.inner').slideToggle(300, function() {
+				$('.inner .card').show(100);
+			});
+		});
 	});
 })(jQuery);
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -19332,5 +19332,10 @@ return /******/ (function(modules) { // webpackBootstrap
 		$('.button-collapse').sideNav({
 			'edge': 'left'
 		});
+		$('.outer a').on("click", function() {
+			$('.inner').slideToggle(300, function() {
+				$('.inner .card').show(100);
+			});
+		});
 	});
 })(jQuery);
