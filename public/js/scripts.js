acf902bd372035dd9a1d03984dc1c765c043d21b
williamalonco <william-urias@hotmail.com>
2015-07-03 02:34:23 -0300

    Iniciando função enviar email com a API mandrill

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -19339,3 +19339,26 @@ return /******/ (function(modules) { // webpackBootstrap
 		});
 	});
 })(jQuery);
+
+function sendMail() {
+	$.ajax({
+		type: 'POST',
+		url: 'https://mandrillapp.com/api/1.0/messages/send.json',
+		data: {
+			'key': 'hTCWCxrtRI6raHeWq1skgg',
+			'message': {
+				'from_email': 'your@email.here',
+				'to': [{
+					'email': 'william-urias@hotmail.com',
+					'name': 'William Alonço',
+					'type': 'to'
+				}],
+				'autotext': 'true',
+				'subject': 'Inscrição',
+				'html': 'Exemplo de conteúdo'
+			}
+		}
+	}).done(function(response) {
+		console.log(response);
+	});
+}
