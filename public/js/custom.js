f90ba98a01fd281243702b331d391dd7c0c9a2a4
williamalonco <william-urias@hotmail.com>
2015-07-05 05:00:53 -0300

    Finalizando função email

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,35 +1,48 @@
 (function($) {
-	$(function() {
-		$('.button-collapse').sideNav({
-			'edge': 'left'
-		});
-		$('.outer a').on("click", function() {
-			$('.inner').slideToggle(300, function() {
-				$('.inner .card').show(100);
-			});
-		});
-	});
+    $(function() {
+        $('.button-collapse').sideNav({
+            'edge': 'left'
+        });
+        $('.outer a').on("click", function() {
+            $('.inner').slideToggle(300, function() {
+                $('.inner .card').show(100);
+            });
+        });
+        $("#send_mail").submit(function() {
+            var name = $("#name").val();
+            var email = $("#email").val();
+            var message = $("#message").val();
+            $.ajax({
+                    type: "POST",
+                    url: "https://mandrillapp.com/api/1.0/messages/send.json",
+                    data: {
+                        'key': 'hTCWCxrtRI6raHeWq1skgg',
+                        'message': {
+                            'from_name': name,
+                            'from_email': email,
+                            'headers': {
+                                'Reply-To': email
+                            },
+                            'subject': '[Meu Treinamento] Estou interessado em saber mais sobre o sistema!',
+                            'text': message,
+                            'to': [{
+                                'email': 'william-urias@hotmail.com',
+                                'name': 'William Alonço',
+                                'type': 'to'
+                            }]
+                        }
+                    }
+                })
+                .done(function(response) {
+                    Materialize.toast('Mensagem enviada com sucesso!', 3500);
+                    $("#name").val('');
+                    $("#email").val('');
+                    $("#message").val('');
+                })
+                .fail(function(response) {
+                    Materialize.toast('Erro ao enviar mensagem!', 3500);
+                });
+            return false;
+        });
+    });
 })(jQuery);
-
-function sendMail() {
-	$.ajax({
-		type: 'POST',
-		url: 'https://mandrillapp.com/api/1.0/messages/send.json',
-		data: {
-			'key': 'hTCWCxrtRI6raHeWq1skgg',
-			'message': {
-				'from_email': 'your@email.here',
-				'to': [{
-					'email': 'william-urias@hotmail.com',
-					'name': 'William Alonço',
-					'type': 'to'
-				}],
-				'autotext': 'true',
-				'subject': 'Inscrição',
-				'html': 'Exemplo de conteúdo'
-			}
-		}
-	}).done(function(response) {
-		console.log(response);
-	});
-}
