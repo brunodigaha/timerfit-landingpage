1f8b40fa9ae2e4214e8ab5d134b042ce0251bca7
williamalonco <william-urias@hotmail.com>
2015-07-09 02:15:15 -0300

    Configuração do browsersync com a task watch e tratamento de erros das tasks jade e sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,39 @@
-var gulp 	= require('gulp'),
-	jade 	= require('gulp-jade'),
-	sass 	= require('gulp-sass'),
-	concat 	= require('gulp-concat');
+var gulp 		= require('gulp'),
+	jade 		= require('gulp-jade'),
+	sass 		= require('gulp-sass'),
+	concat 		= require('gulp-concat'),
+	browserSync = require('browser-sync'),
+	reload 		= browserSync.reload;
 
+// Task Jade
 gulp.task('jade', function() {
-	gulp.src('./public/template/**/index.jade')
+	gulp.src('./public/templates/**/index.jade')
 		.pipe(jade({
 			pretty: true
 		}))
+		.on('error', function(error) {
+			console.error('' + error);
+		})
 		.pipe(gulp.dest('./public/'))
+		.pipe(reload({
+			stream: true
+		}));
 });
 
+// Task Sass
 gulp.task('sass', function() {
 	gulp.src('./public/css/sass/**/*.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./public/css'));
+		.pipe(sass())
+		.on('error', function(error) {
+			console.error('' + error);
+		})
+		.pipe(gulp.dest('./public/css'))
+		.pipe(reload({
+			stream: true
+		}));
 });
 
+// Task JS
 gulp.task('js', function() {
 	return gulp.src(['./public/bower_components/modernizr/modernizr.js', './public/bower_components/jquery/dist/jquery.js', './public/bower_components/materialize/dist/js/materialize.js', './public/bower_components/holderjs/holder.js', 'public/js/custom.js'])
 		.pipe(concat({
@@ -25,13 +42,18 @@ gulp.task('js', function() {
 				mode: 0666
 			}
 		}))
-		.pipe(gulp.dest('./public/js/'));
+		.pipe(gulp.dest('./public/js/'))
+		.pipe(reload({
+			stream: true
+		}));
 });
 
-gulp.task('watch', function() {
-	gulp.watch('./public/template/**/*.jade', ['jade']);
+// Task Default 
+gulp.task('default', ['jade', 'sass', 'js'], function() {
+	browserSync({
+		server: './public/'
+	});
+	gulp.watch('./public/templates/**/*.jade', ['jade']);
 	gulp.watch('./public/css/sass/**/*.scss', ['sass']);
 	gulp.watch('./public/js/**/*.js', ['js']);
 });
-
-gulp.task('default', ['jade', 'sass', 'js', 'watch']);
