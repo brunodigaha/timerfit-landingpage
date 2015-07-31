6a882100886a009b73bde2348abebd4d442f6c58
williamalonco <william-urias@hotmail.com>
2015-07-31 01:45:33 -0300

    Estilização do template hero-unit e inserção das libs google-material-colors, jquery e google webfont.js

---
+++ public/js/custom.js
@@ -0,0 +1,15 @@
+ // Google Web Fonts
+ WebFontConfig = {
+   google: {
+     families: ['Roboto:400,100,100italic,300,300italic,500,400italic,500italic,700,700italic,900,900italic:latin']
+   }
+ };
+ (function() {
+   var wf = document.createElement('script');
+   wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+     '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+   wf.type = 'text/javascript';
+   wf.async = 'true';
+   var s = document.getElementsByTagName('script')[0];
+   s.parentNode.insertBefore(wf, s);
+ })();
