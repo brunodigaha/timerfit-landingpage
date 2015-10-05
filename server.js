88adf53c69b3e8f51267e7a2d4b4200cfb58a642
williamalonco <william-urias@hotmail.com>
2015-10-05 19:18:25 -0300

    Modificações no server.js, atualização hapi.js, inserção da dependência inert e deploy heroku

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
 var Hapi = require('hapi');
+var Inert = require('inert');
 
 var server = new Hapi.Server();
 
-server.connection({
-	host: '0.0.0.0',
-	port: parseInt(process.env.PORT, 10) || 3000
-});
-
-server.route({
-	method: 'GET',
-	path: '/{param*}',
-	handler: {
-		directory: {
-			path: 'public',
-			listing: true
+server.register(Inert, function() {
+	server.connection({
+		host: '0.0.0.0',
+		port: parseInt(process.env.PORT, 10) || 3000
+	});
+	server.route({
+		method: 'GET',
+		path: '/{param*}',
+		handler: {
+			directory: {
+				path: 'public',
+				listing: true
+			}
 		}
-	}
+	});
 });
 
-server.start(function () {
+server.start(function() {
 	console.log('Hapi server started: ' + server.info.uri);
 });
